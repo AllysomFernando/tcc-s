bd50c72d0bdd967f8e754e4b813df78205f7af93
Allysom Renczenczen <82781674+AllysomFernando@users.noreply.github.com>
2021-07-13 18:51:01 -0300

    index
    atualizei o nosso header e estou implentando novas coiass

--- index.php
+++ index.php
@@ -6,9 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ruddy Store - Loja de jogos </title>
+   <!--fontawesom link abaixo-->  
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous"> 
+   <!-- google fonts abaixo -->   
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap" rel="stylesheet">
+   <!-- css abaixo-->
     <link href="estilo/style.css" rel="stylesheet"/>
     <!--SEO-->
     <meta name="description" content="Ruddy Store - Todos os tipos de jogos e afins você encontra aqui"/>
@@ -23,21 +27,23 @@
     <title>HOME</title>
 </head>
 <body>
-<header>
-        
-        <div class="logo left"> <img src="imagens/logo.png "width="100px"></div><!--ficara a logo-->
-       
-        <nav class="desktop right">
+<header>   
+        <a href="#" class="logo"><img src="imagens/logo.png" width="100px"></a><!--ficara a logo-->
+        <nav class="desktop">
             <ul>
-                <li><a href="">JOGOS</a></li>
-                <li><a href="">MEUS CÓDIGOS</a></li>
-                <li><a href="">MINHA CARTEIRA</a></li>
-                <li><a href="">SOBRE NÓS</a></li>
-                <li><a href="">LOGIN</a></li>
-                <li><a href="">REGISTRO</a></li>
+                <li><a href="#">JOGOS</a></li>
+                <li><a href="#">MEUS CÓDIGOS</a></li>
+                <li><a href="#">MINHA CARTEIRA</a></li>
+                <li><a href="#">LOGIN</a></li>
             </ul>
         </nav>
-         <nav class="mobile right">
+        <nav class="search">
+        <div>
+          <input type="text" placeholder="What're ya buying?">
+          <i class="fas fa-search"></i><!-- icone de procura-->
+        </div> 
+        </nav>  
+         <nav class="mobile">
             <ul>
                 <li><a href="">JOGOS</a></li>
                 <li><a href="">MEUS CÓDIGOS</a></li>
@@ -47,14 +53,13 @@
                 <li><a href="">REGISTRO</a></li>
             </ul>
         </nav>    
-</div>  
-<div class="clear"></div>
 </header>
-<section class="freewknd">
-<div class="left">
-  <div class="mainbanner">
-    <img src="imagens/cyberpunk.jpg" width="200px">
-</div>  
+<div class="container">
+  <div class="content">
+   <a href="#"> <img src="imagens/cyberpunk.jpg" class="cyber" width="1000px"></a>
+  </div>
+</div>   
+<!--
   </div>
   <div class="center right"> 
   <div class="sales1">
@@ -62,11 +67,13 @@
   </div>
   <div class="sales2">
   <img src="imagens/watchdogs.jpg" width="200px">
-  </div>
+  </div>  
 </div>
 <div class="clear"></div>
 </section><!--parte da freewknd-->
+<!--
 <section class="allgames"><!--outros games-->
+<!--
   <div class="center">
 <div class="game1 ">
   <img src="imagens/revilage.jpg" width="200px">
