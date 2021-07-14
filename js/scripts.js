f79b1d174ff3e872b02a63c43b28153e0d107889
Allysom Renczenczen <82781674+AllysomFernando@users.noreply.github.com>
2021-07-13 21:49:32 -0300

    atualização js menu
    incrimentei js no menu mobile

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,14 @@ $(function(){
     //Aqui vou colocar todo o nosso codigo de javascript
     $('nav.mobile').click(function(){
         //o que vai acontecer quando clicar na nav mobile
-        var listamenu = $ ('nav.mobile ul');
-        listamenu.fadIn();
+        var listaMenu = $('nav.mobile ul');
+        if(listaMenu.is(':hidden') == true)
+            listaMenu.fadeIn();
+        else
+            listaMenu.fadeOut();
+        
+    
+            
     })
 })
 
