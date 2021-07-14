3a8466e24d172d5fa16bbd978bace1ff6a208196
Victor Hugo <83251487+PurpIeMango@users.noreply.github.com>
2021-07-14 16:12:34 -0300

    update

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap" rel="stylesheet">
    <!-- css abaixo-->
-    <link href="estilo/styleteste.css" rel="stylesheet"/>
+    <link href="estilo/style.css" rel="stylesheet"/>
     <!--SEO-->
     <meta name="description" content="Ruddy Store - Todos os tipos de jogos e afins você encontra aqui"/>
     <!--SMO-->
