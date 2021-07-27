b439271909b7cee3cbbf35318b2c1fc8138938f1
Allysom Renczenczen <82781674+AllysomFernando@users.noreply.github.com>
2021-07-27 15:09:17 -0300

    update

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- icone -->
+    <link rel="icon" href="<?php echo INCLUDE_PATH; ?>logo.ico" type="image/x-icon" />
     <title>Ruddy Store - Loja de jogos </title>
     <!--fontawesom link abaixo-->  
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous"> 
