1bfcf4ca460c776e4bd0190b7628ff0f4b9e81a9
Allysom Renczenczen <82781674+AllysomFernando@users.noreply.github.com>
2021-11-05 21:05:13 -0300

    update

--- pages/home.php
+++ pages/home.php
@@ -31,37 +31,6 @@
 
 <hr class="hr5">
 
-<section class="allgames">
-  <!--outros games-->
-
-
-
-  <div class="game1 ">
-    <a href="#"><img src="<?php echo INCLUDE_PATH; ?>imagens/revilage.jpg" class="revi" width="800px"></a>
-    <p class="revi_desc">Continuando de onde o risco biológico de Resident
-      Evil 7 parou, Resident Evil Village é o oitavo grande episódio da série Resident Evil. Tecnologia de
-      Próxima Geração RE Engine emparelhado com poder de console de próxima
-      geração fornecerá gráficos fotorrealistas, trazendo a aldeia sombria e seus
-      residentes assombrados para a vida.</p>
-  </div>
-
-  <!--
-
-<div class="game2"> 
-  <a href="#"><img src="<?php echo INCLUDE_PATH; ?>imagens/skyrim.jpg" class="" width="200px"></a>
-</div>
-
--->
-  <!--
-<div class="game3">
-<img src="<?php echo INCLUDE_PATH; ?>imagens/cyberpunk.jpg" width="200px">
-</div>
-<div class="gamer4">
-<img src="<?php echo INCLUDE_PATH; ?>imagens/farcry5.jpg" width="200px">
-</div>
-</div>
-</section>
--->
 
 
 <section class="allgames"><!--outros games-->
