01b0571ac07cc929b3828dfeabeeb71eac639b2b
Victor Hugo <83251487+PurpIeMango@users.noreply.github.com>
2021-11-19 22:45:50 -0300

    Update jogos.php

--- pages/jogos.php
+++ pages/jogos.php
@@ -2,11 +2,17 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-widthm, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="style_products.css" media="screen" />
+  <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
 </head>
 
 <body>
-  <div class="container">
-      
+  <br><br><br><br><br><br><br><br><br><br>
+  <br><br><br><br><br><br><br><br><br><br>
+  <br><br><br><br><br><br><br><br><br><br>
+  <br><br><br><br><br><br><br><br><br><br>
+      <div class="jogo1">
+      <p class="destaques">JOGOS</p>
+<hr class="hr0">
+      </div>
   </div>
 </body>
