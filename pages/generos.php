6e638bc01ac10c0ccbea2d7e4bf2fd3b93a6c796
Victor Hugo <83251487+PurpIeMango@users.noreply.github.com>
2021-11-22 11:57:02 -0300

    Create generos.php

---
+++ pages/generos.php
@@ -0,0 +1,23 @@
+<br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br>
+     
+<body>
+<b><a class="ter" href="jogos">JOGOS DE TERROR</a></b>
+
+<div class="gen">
+    <hr class="hr11">
+  
+    <br><br><br><br><br><br>
+    <b><a class="ter" href="jogos">JOGOS DE AÇÃO</a></b>
+
+    <hr class="hr12">
+   
+    <br><br><br><br><br>
+    <br>
+    <b><a class="ter" href="jogos">JOGOS DE CORRIDA</a></b>
+
+    <hr class="hr13">
+ 
+    <br><br><br><br><br><br><br><br>
+</div>
+</body>
